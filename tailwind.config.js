94d39787f13aed7f64531733204944f181735cfc
Marc Hawkins <marc.hawkins@sideshow.com>
2021-03-20 00:46:01 -0400

    node fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {
-      fontFamily: {
-        'title': ['NeilvardOne','Arial'],
-        'hand': ['PermanentMarker-Regular','Arial'],
-      }
+    fontFamily: {
+      'title': ['NeilvardOne','Arial'],
+      'mono': ['VT323','Arial'],
+      'hand': ['PermanentMarker-Regular','Arial'],
     },
-  },
-  variants: {
-    margin: ['responsive','even','odd','first','last','hover','focus'],
-    padding: ['responsive','even','odd','first','last','hover','focus'],
-    backgroundColor:  ['responsive','even','odd','first','last','hover','focus'],
-  },
-  plugins: [],
+    extend: {
+      variants: {
+        margin: ['responsive','even','odd','first','last','hover','focus'],
+        padding: ['responsive','even','odd','first','last','hover','focus'],
+        backgroundColor:  ['responsive','even','odd','first','last','hover','focus'],
+      }
+    }
+  }
 }
